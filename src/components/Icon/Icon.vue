26b89c9dfab84e3697467ad3f946b6163eabc94c
ZhangMin1998 <1464253423@qq.com>
2023-07-11 00:49:41 +0800

    svg icon

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg :style="{...style}">
+    <use :href="iconName"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Icon',
+  props: {
+    name: String,
+    style: Object
+  },
+  setup(props) {
+    const iconName = `#${props.name}`
+
+    return {
+      iconName
+    }
+  }
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Test from './components/Test/index'
 import Test2 from './components/Test2/index'
 import Test3 from './components/Test3/index'
+import Icon from './components/Icon/index'
 
 export default function(Vue) {
   // Vue.component(Test.name, Test)
   Vue.use(Test)
   Vue.use(Test2)
   Vue.use(Test3)
+  Vue.use(Icon)
 }
